9c57890386da205e93dc23f3b1bd7fd5a47b261d
Gary Yue <garyyue2017@gmail.com>
2019-03-15 22:12:57 -0700

    second

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 const app = express();
 
 const db = process.env.MONGO_URI
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const song = require('./routes/api/song');
 const user = require('./routes/user/user');
